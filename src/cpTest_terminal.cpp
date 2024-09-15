edffca23dd36464a336fc2988ce33553d142b90e
madbanana22 <madbanana100@gmail.com>
2024-09-15 11:48:19 +0700

    le epic initial commit

---
+++ src/cpTest_terminal.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+// stinky stinky using namespace std;
+
+int main()
+{
+	std::cout << "Holy Hell! This shit works!" << std::endl; // Your daily reminder to google en passant
+	return 0;
+}
